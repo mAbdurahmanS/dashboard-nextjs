06f861cd46058d672bd9f485ee19914b67f63f2f
Muhammad Abdurahman Salsabil <abil7530@gmail.com>
2023-05-23 08:16:23 +0700

    Update

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,11 @@
+import sidebar from '@/components/sidebar'
 import '@/styles/globals.css'
+import Sidebar from '@/components/Sidebar'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+  <Sidebar>
+    <Component {...pageProps} />
+  </Sidebar>
+  )
 }
